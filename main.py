bf3c09bd14f106244bd36217bd67870617f075d2
eduardoparracho <eduardorganist@gmail.com>
2024-10-11 19:53:47 +0100

    ZipParser implemented

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from scrapper import CensusExtractor
+from zip_parser import ZipParser
 
 def main():
+    
     zip_dir = 'zipfiles'
+    unzip_dir = 'extraction'
+    
     extractor = CensusExtractor(zip_dir)
     
     if not extractor.run():
         print("Terminating execution")
         return
+    
+    unzipper = ZipParser(unzip_dir)
+    unzipper.run(zip_dir, extractor.get_zip_list())
     
     
     
